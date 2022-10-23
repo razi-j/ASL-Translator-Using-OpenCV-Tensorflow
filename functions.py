66404e81c2908c54266630fd43fe681989adfc33
broonjames <113360730+broonjames@users.noreply.github.com>
2022-10-23 16:18:46 +0800

    model trained + load weights method added to vm class

--- functions.py
+++ functions.py
@@ -59,3 +59,21 @@ class VM:
         a = np.concatenate([lh, rh, pose, face])
         return a
 
+
+    @classmethod
+    def load_weights(cls):
+        # model rebuild
+        model = Sequential()
+        model.add(LSTM(64, return_sequences="True", activation="relu", input_shape=(30, 1662)))
+        model.add(LSTM(128, return_sequences="True", activation="relu"))
+        model.add(LSTM(64, return_sequences="False", activation="relu"))
+        model.add(Flatten(input_shape=(x_train.shape[1:])))
+        model.add(Dense(32, activation="relu"))
+        model.add(Dense(64, activation="relu"))
+        model.add(Dense(np.array(signs).shape[0], activation="softmax"))
+
+        # compiler
+        model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["categorical_accuracy"])
+
+        # load
+        model.load_weights('VertoMotus_MLmodel.h5')
