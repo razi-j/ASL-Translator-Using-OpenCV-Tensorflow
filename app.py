dd09fa27e67c76ee3e49061218256b0a2e54df41
quacks <razaljosephandrew@gmail.com>
2022-11-19 19:09:13 +0800

    FIXED THE LOGIC KINDLY MERGE

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ import tensorflow as tf
 def main():
     # Needed Variables for Detection
     sequence =[]
-    threshold = 0.7
+    threshold = 0.9
     sentence =[]
     predictions = []
-    #signs = sorted(os.listdir("Data/"))
+    signs = sorted(os.listdir("try_data/"))
     # Initialization of TFLite Model
-    #interpreter = tf.lite.Interpreter(model_path="VertoMotus3.tflite")
-    #interpreter.allocate_tensors()
-
-   
+    interpreter = tf.lite.Interpreter(model_path="normalized_data.tflite")
+    interpreter.allocate_tensors()
+    input_d = interpreter.get_input_details()
+    output_d = interpreter.get_output_details()
+    
 
     cap = cv.VideoCapture(0)
     with VM.mp_holistic.Holistic(min_detection_confidence= 0.5, min_tracking_confidence= 0.5) as holistic:
@@ -32,19 +33,19 @@ def main():
             # displays camera feed with landmarks
             
             keypoints = VM.extract_keypoints(results) # Extract Keypoints to be used to predict actions
-            #sequence.append(keypoints) # Append Keypoints to Variable
-            #sequence = sequence[-30:]
+            sequence.append(keypoints) # Append Keypoints to Variable
+            sequence = sequence[-30:]
 
-            #input_d = interpreter.get_input_details()
-            #output_d = interpreter.get_output_details()
 
-            #if len(sequence) == 30:
-            #    interpreter.set_tensor(input_d[0]["index"], np.array(np.expand_dims(sequence,axis=0), dtype=np.float32))
-            #    interpreter.invoke()
-            #    output_data = interpreter.get_tensor(output_d[0]["index"])
-            #    res = np.argmax(np.squeeze(output_data))
-            #    print(signs[res])
-                
+            if len(sequence) == 30:
+                interpreter.set_tensor(input_d[0]["index"], np.array(np.expand_dims(sequence,axis=0), dtype=np.float32))
+                interpreter.invoke()
+                output_data = interpreter.get_tensor(output_d[0]["index"])
+                pred  = np.squeeze(output_data)
+                for i in pred:
+                    if i >= threshold:
+                        res = np.argmax(np.squeeze(output_data))
+                        print(signs[res])
 
             end = time.time()
             fps = 1/(end-start)
