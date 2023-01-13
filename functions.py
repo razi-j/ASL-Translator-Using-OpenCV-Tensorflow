78f14b1c241e26c93dd70392a70b255be7d48f7a
broonjames <baronocasiones@gmail.com>
2023-01-14 06:05:39 +0800

    datas
    please, what is your name, you are beautiful, you're welcome

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ class VM:
     mp_draw = mp.solutions.drawing_utils
     DATA_PATH = os.path.join("./Keypoint_Data")
 
-    fsl = np.array(["i'm sorry"]) #things to put here: asl words, phrases
+    fsl = np.array(["you're welcome"]) #things to put here: asl words, phrases
     seq = 50 # number of videos to be used for data collection
     seq_lenght = 30 # number of frames to be used per video
 
