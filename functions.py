1131d63705255a53011f2744f7f6943d6922e06f
Jaja <83009990+razi-j@users.noreply.github.com>
2023-04-04 06:27:33 +0800

    Nice To Meet You

--- functions.py
+++ functions.py
@@ -12,7 +12,7 @@ class VM:
     mp_draw = mp.solutions.drawing_utils
     DATA_PATH = os.path.join("./Keypoint_Data")
 
-    fsl = np.array(["take care"]) #things to put here: asl words, phrases
+    fsl = np.array(["nice to meet you"]) #things to put here: asl words, phrases
     seq = 50 # number of videos to be used for data collection
     seq_lenght = 30 # number of frames to be used per video
 
