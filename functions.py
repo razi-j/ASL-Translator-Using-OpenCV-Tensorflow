d3d243a6da8e656a1f2b75188ff09dbf3f0279e6
razi-j <razaljosephandrew@gmail.com>
2023-04-17 18:55:25 +0800

    New Data. 100 Videos.

--- functions.py
+++ functions.py
@@ -12,8 +12,8 @@ class VM:
     mp_draw = mp.solutions.drawing_utils
     DATA_PATH = os.path.join("./Keypoint_Data")
 
-    fsl = np.array(["nice to meet you"]) #things to put here: asl words, phrases
-    seq = 50 # number of videos to be used for data collection
+    fsl = np.array(["youre welcome"]) #things to put here: asl words, phrases
+    seq = 100 # number of videos to be used for data collection
     seq_lenght = 30 # number of frames to be used per video
 
     def build_folder():
@@ -42,18 +42,22 @@ class VM:
                                 VM.mp_draw.DrawingSpec(color=(84, 44, 44), thickness=2, circle_radius=3),
                                 VM.mp_draw.DrawingSpec(color=(255,255,255), thickness=2, circle_radius=1))
         
-        #VM.mp_draw.draw_landmarks(image, results.face_landmarks, VM.mp_holistic.FACEMESH_TESSELATION,
-        #                        VM.mp_draw.DrawingSpec(color=(255,170,170), thickness=1, circle_radius=1),
-        #                        VM.mp_draw.DrawingSpec(color=(255,255,255), thickness=1, circle_radius=1))
+        VM.mp_draw.draw_landmarks(image, results.face_landmarks, VM.mp_holistic.FACEMESH_TESSELATION,
+                                VM.mp_draw.DrawingSpec(color=(255,170,170), thickness=1, circle_radius=1),
+                                VM.mp_draw.DrawingSpec(color=(255,255,255), thickness=1, circle_radius=1))
+        VM.mp_draw.draw_landmarks(image, results.pose_landmarks, VM.mp_holistic.POSE_CONNECTIONS,
+                             VM.mp_draw.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             VM.mp_draw.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             )
 
     def extract_keypoints(results):
         # list comprehension to loop over results and get needed data, then arranged to np.array. flattened to turn it into one array. else is to make a placeholder
         lh = np.array([[res.x,res.y,res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
         rh = np.array([[res.x,res.y,res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
         face = np.array([[res.x,res.y,res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
-        
+        pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
         # puts all data into one array
-        a = np.concatenate([face, lh, rh])
+        a = np.concatenate([face, lh, rh, pose])
 
         lm_list = []
 
